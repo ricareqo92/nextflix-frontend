847de6170783e8773cbf33d32ee073f61d5e91dd
Luis Leon <luis.leon@sofoscorp.com>
2021-01-21 14:36:02 -0400

    netflix page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-import { HomeComponent } from './pages/home/home.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { SearchComponent } from './pages/search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
     BannerComponent,
     SearchComponent
   ],
